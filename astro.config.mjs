e6171cdbdc64638e1ce157f82188008c92171dc8
Raul Ospina <raminka13@hotmail.com>
2025-02-09 11:40:57 -0500

    Update: add site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://anfassa.com",
   integrations: [react(), sitemap()],
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
